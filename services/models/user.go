ef4ff182213c824481cc2140c482c75383dc2f16
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2017-09-02 21:54:42 +0200

    Added class group model

--- services/models/user.go
+++ services/models/user.go
@@ -1,19 +1,19 @@
 package models
 
 type User struct {
-	Id        	int	`db:"id, omitempty"`
-	Username  	string	`db:"username, omitempty"`
-	Password  	string	`db:"password, omitempty"`
-	UserRole 	int	`db:"user_role, omitempty"`
-	FirstName	string	`db:"first_name, omitempty"`
-	LastName	string	`db:"last_name, omitempty"`
-	Address 	string	`db:"address, omitempty"`
-	City		string	`db:"city, omitempty"`
-	ZipCode		int	`db:"zipcode, omitempty"`
-	Email 		string	`db:"email, omitempty"`
+	Id        int    `db:"id, omitempty"`
+	Username  string `db:"username, omitempty"`
+	Password  string `db:"password, omitempty"`
+	UserRole  int    `db:"user_role, omitempty"`
+	FirstName string `db:"first_name, omitempty"`
+	LastName  string `db:"last_name, omitempty"`
+	Address   string `db:"address, omitempty"`
+	City      string `db:"city, omitempty"`
+	ZipCode   int    `db:"zipcode, omitempty"`
+	Email     string `db:"email, omitempty"`
 }
 
 type UserRoles struct {
-	Id		int	`db:"id"`
-	Role_name	string	`db:"role_name"`
+	Id        int    `db:"id"`
+	Role_name string `db:"role_name"`
 }
