441627f8bd610c473897bb86f2f0fd22b955139e
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2017-09-02 21:31:57 +0200

    Added student model

---
+++ services/models/student.go
@@ -0,0 +1,14 @@
+package models
+
+type Student struct {
+	ID         int    `db:"id, omitempty"`
+	FirstName  string `db:"first_name, omitempty"`
+	LastName   string `db:"last_name, omitempty"`
+	Address    string `db:"address, omitempty"`
+	City       string `db:"city, omitempty"`
+	ZipCode    int    `db:"zipcode, omitempty"`
+	Email      string `db:"email, omitempty"`
+	Phone      string `db:"phone, omitempty"`
+	EnrollDate string `db:"enrolldate, omitempty"`
+	Parent     int    `db:"parent, omitempty"`
+}
