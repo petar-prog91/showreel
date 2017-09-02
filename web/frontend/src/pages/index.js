c0a5701a2548ae9ff1caabbe0d7510e1918b3a79
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2017-09-02 22:45:30 +0200

    Added classes page

--- web/frontend/src/pages/index.js
+++ web/frontend/src/pages/index.js
@@ -1,5 +1,6 @@
 export App from './app';
 export Home from './home/home';
 export Settings from './settings/settings';
+export Classes from './classes/classes';
 export Login from './login/login';
 export NotFound from './notfound/notfound';
