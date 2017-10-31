375763aa22ab48385cd435976bea439ceef13773
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2017-10-31 16:04:24 +0100

    Small info added per episode

--- frontend/src/components/showseasons/showseasons.js
+++ frontend/src/components/showseasons/showseasons.js
@@ -4,7 +4,7 @@ import { PropTypes } from 'prop-types';
 const ShowSeasons = ({ data }) => (
     <div className="show__showepisodes">
         <ul>
-            { data.seasons.map((seasons) => seasons.map((episode) => <li key={episode.name}>{episode.name}</li>)) }
+            { data.seasons.map((seasons) => seasons.map((episode) => <li key={episode.name}>{episode.number}. {episode.name} - {episode.airdate}</li>)) }
         </ul>
     </div>
 );
