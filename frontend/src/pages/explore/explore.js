3dc0770a1684be0491abb5584d44aa5dfb261f2a
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2017-11-03 22:28:55 +0100

    Some change to explore page and hardcoding some dummy data

--- frontend/src/pages/explore/explore.js
+++ frontend/src/pages/explore/explore.js
@@ -1,12 +1,38 @@
 
 import React from 'react';
 import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { TV_MAZE_API } from '../../constants';
+import statusHandle from '../../utils/statusHandle';
 
-const Explore = () => (
-    <div>
-        <h1>Explore page</h1>
-        <p>Show list</p>
-    </div>
-);
+import ShowList from '../../components/showlist/showlist';
+import { loadShows } from '../../actions/shows';
 
-export default connect()(Explore);
+class Explore extends React.Component {
+    componentDidMount() {
+        fetch(`${TV_MAZE_API + '/schedule?country=US&date=2014-12-01'}`)
+        .then(statusHandle)
+        .then(response => response.json())
+        .then(response => this.props.loadShows(response))
+        .catch(error => Promise.reject(error));
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Explore Page</h1>
+                <ShowList listOfShows={this.props.showsList} />
+            </div>
+        );
+    }
+}
+
+Explore.propTypes = {
+    showsList: PropTypes.array.isRequired,
+    loadShows: PropTypes.func.isRequired,
+};
+
+export default connect(
+    state => ({ showsList: state.showsState.showsList }),
+    { loadShows },
+)(Explore);
