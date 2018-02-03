c45a835d0957e0e14ec3c8bbb229f2ec13ad1985
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2018-02-03 16:55:34 +0100

    Removed package lock, useless

--- APP/app.js
+++ APP/app.js
@@ -1,21 +1,22 @@
 const port = 4040;
-const spdy = require('spdy')
-const express = require('express')
-const path = require('path')
-const fs = require('fs')
+const spdy = require('spdy');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
-const app = express()
+const app = express();
 
 app.get('*', (req, res) => {
     res
       .status(200)
       .json({message: 'ok'})
-})
+});
+
 const options = {
     key: fs.readFileSync(__dirname + '/server.key'),
     cert:  fs.readFileSync(__dirname + '/server.crt')
-}
-console.log(options)
+};
+
 spdy
   .createServer(options, app)
   .listen(port, (error) => {
@@ -25,4 +26,4 @@ spdy
     } else {
       console.log('Listening on port: ' + port + '.')
     }
-  })
+  });
