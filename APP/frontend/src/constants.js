f2bd35c215447f46c89ed97b590a69d75a7289e8
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2018-02-03 15:42:49 +0100

    Restructuring the whole repo to include both APP and API

---
+++ APP/frontend/src/constants.js
@@ -0,0 +1,12 @@
+
+export const TOGGLE_MAINBAR = 'TOGGLE_MAINBAR';
+export const LOG_IN = 'LOG_IN';
+export const LOG_OUT = 'LOG_OUT';
+export const FETCH_JWT_TOKEN = 'FETCH_JWT_TOKEN';
+export const SAVE_JWT_TOKEN = 'SAVE_JWT_TOKEN';
+export const SHOWREEL_API = 'http://localhost:9090/api/';
+export const TV_MAZE_API = 'http://api.tvmaze.com';
+export const REGISTER_USER = 'REGISTER_USER';
+export const SHOWS_LOAD = 'SHOWS_LOAD';
+export const SHOW_EPISODES_LOAD = 'SHOW_EPISODES_LOAD';
+export const SEARCH_SHOW = 'SEARCH_SHOW';
