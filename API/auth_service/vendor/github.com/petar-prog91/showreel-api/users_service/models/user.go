7bc8b27f71f9ed21e662c22467ddd191f975661a
Petar Milutinovic <petar.milutinovic@netcentric.biz>
2018-02-03 16:17:35 +0100

    Dep init and removing vendor folders from API folder

--- API/auth_service/vendor/github.com/petar-prog91/showreel-api/users_service/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	Id       int    `db:"id, omitempty"`
-	Username string `db:"username, omitempty"`
-	Password string `db:"password, omitempty"`
-	Email    string `db:"email, omitempty"`
-}
